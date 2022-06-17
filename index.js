3fa7ea9716cd478f128d70bb07261d325b1b5d12
mutemip <paulmutemi2030@gmail.com>
2022-06-17 23:27:12 +0300

    test funtions passed

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function findMatching(source, sought) {
-    return source.filter(
-      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
-    );
-  }
-  
-  function fuzzyMatch(source, testString) {
-    return source.filter(
-      (possibleMatch) =>
-        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-    );
-  }
-  
-  function matchName(source, soughtName) {
-    return source.filter((record) => record.name === soughtName);
-  }
+  return source.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  );
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(source, soughtName) {
+  return source.filter((record) => record.name === soughtName);
+}
